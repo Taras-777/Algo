4001e130fbceb7d445e43f08a2fcda00fbb7b813
Taras-777 <74727526+Taras-777@users.noreply.github.com>
2022-06-09 18:37:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-def dijkstra(graph, src):
-    visited = []
-    distance = {src: 0}
-    node = list(range(len(graph[0])))
-    if src in node:
-        node.remove(src)
-        visited.append(src)
-    else:
-        return None
-    for i in node:
-        distance[i] = graph[src][i]
-    prefer = src
-    while node:
-        _distance = float('inf')
+from queue import PriorityQueue
 
+class Graph:
+    def __init__(self, num_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
 
-        for i in visited:
-            for j in node:
-                if graph[i][j] > 0:
-                    if _distance > distance[i] + graph[i][j]:
-                        _distance = distance[j] = distance[i] + graph[i][j]
-                        prefer = j
-        visited.append(prefer)
-        node.remove(prefer)
-    return distance
+    def add_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
 
-graph_list = [[0, 2, 1, 4, 5, 1],
-              [1, 0, 4, 2, 3, 4],
-              [2, 1, 0, 1, 2, 4],
-              [3, 5, 2, 0, 3, 3],
-              [2, 4, 3, 4, 0, 1],
-              [3, 4, 7, 3, 1, 0]]
+    def dijkstra(self, start_vertex):
+        D = {v: float('inf') for v in range(self.v)}
+        D[start_vertex] = 0
 
-if __name__ == '__main__':
-    distance = dijkstra(graph_list, 0)
-    print(distance)
+        pq = PriorityQueue()
+        pq.put((0, start_vertex))
+
+        while not pq.empty():
+            (dist, current_vertex) = pq.get()
+            self.visited.append(current_vertex)
+
+            for neighbor in range(self.v):
+                if self.edges[current_vertex][neighbor] != -1:
+                    distance = self.edges[current_vertex][neighbor]
+                    if neighbor not in self.visited:
+                        old_cost = D[neighbor]
+                        new_cost = D[current_vertex] + distance
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            D[neighbor] = new_cost
+        return D
+
+g = Graph(9)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 6, 7)
+g.add_edge(1, 6, 11)
+g.add_edge(1, 7, 20)
+g.add_edge(1, 2, 9)
+g.add_edge(2, 3, 6)
+g.add_edge(2, 4, 2)
+g.add_edge(3, 4, 10)
+g.add_edge(3, 5, 5)
+g.add_edge(4, 5, 15)
+g.add_edge(4, 7, 1)
+g.add_edge(4, 8, 5)
+g.add_edge(5, 8, 12)
+g.add_edge(6, 7, 1)
+g.add_edge(7, 8, 3)
+
+D = g.dijkstra(0)
+print(D)
